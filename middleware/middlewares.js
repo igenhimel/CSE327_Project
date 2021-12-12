804655ef97a5d03ad5c5d707d769d752b5effec3
ABmaxplunck <asifulbijoy97@gmail.com>
2021-12-12 19:57:02 +0600

    middleware

---
+++ middleware/middlewares.js
@@ -0,0 +1,41 @@
+
+const express = require('express')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session);
+const config = require('config')
+
+const MONGODB_URI=`mongodb+srv://${config.get('db-admin')}:${config.get('db-password')}@cluster0.13eyw.mongodb.net/CSE327`
+
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+  });
+
+  /**
+   * All Middleware 
+   */
+const middleware = [
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({extended:true}),
+    express.json(),
+    session({
+        secret:'keyboard cat',
+        resave:false,
+        saveUninitialized:false,
+        store:store,
+        cookie : {
+            maxAge: 1000* 60 * 60 *24 * 365
+        }
+    })
+   
+    
+
+]
+
+module.exports = app =>{
+    middleware.forEach(m=>{
+        app.use(m)
+    })
+}
