012142acf202661da464b6d087b68b27999fe78d
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-13 16:28:58 +0600

    flashmessage middleware and documentation added

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
 const config = require('config')
-
+const flash = require('connect-flash')
 
 const MONGODB_URI=`mongodb+srv://${config.get('db-admin')}:${config.get('db-password')}@cluster0.13eyw.mongodb.net/CSE327`
 
 /**
- * All middleware
+ * All middlewares
  */
 const middleware = [
     morgan('dev'),
     express.static('public'),
     express.urlencoded({extended:true}),
-    express.json()
+    express.json(),
+    flash()
     
 
 ]
