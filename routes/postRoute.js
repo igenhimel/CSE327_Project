2d2ddb6c8b93ac2720a4b8be9e29520c75b3b9fc
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-14 22:16:50 +0600

    create post view route added

---
+++ routes/postRoute.js
@@ -0,0 +1,15 @@
+const post = require('express').Router()
+
+const {
+    createPostGet
+} = require('../controllers/postController')
+
+const upload = require('../middleware/UploadMiddleware')
+ 
+/**
+ * All routes for API POST.
+ */
+post.get('/createPost',createPostGet)
+
+ 
+module.exports=post
