d19c75dd548061f917341624ab3bdbb6094fdb14
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-14 21:45:32 +0600

    all middleware updated

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -1,19 +1,43 @@
 const express = require('express')
 const morgan = require('morgan')
-const config = require('config')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session);
+const {bindUserWithRequest}= require('../middleware/authMiddleware')
+const {setLocals} = require('../middleware/setLocals')
 const flash = require('connect-flash')
- 
-const MONGODB_URI=`mongodb+srv://${config.get('db-admin')}:${config.get('db-password')}@cluster0.13eyw.mongodb.net/CSE327`
+const config = require('config')
+const passport = require('passport')
 
-/**
- * All middlewares
- */
+
+
+require('../config/passport')(passport)
+
+const MONGODB_URI=`mongodb+srv://${config.get('db-admin')}:${config.get('db-password')}@cluster0.13eyw.mongodb.net/diary`
+
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions',
+  });
+
 const middleware = [
     morgan('dev'),
     express.static('public'),
     express.urlencoded({extended:true}),
     express.json(),
-    flash()
+    session({
+        secret:'keyboard cat',
+        resave:false,
+        saveUninitialized:false,
+        store:store,
+        cookie : {
+            maxAge: 1000* 60 * 60 *24 * 365
+        }
+    }),
+    flash(),
+    bindUserWithRequest(),
+    setLocals(),
+    passport.initialize(),
+    passport.session()
     
 
 ]
