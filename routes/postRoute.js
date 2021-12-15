5e502ffc05274802a19275db6fb6a24e0be65fa8
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-15 20:38:35 +0600

    post creation route updated

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,15 +1,20 @@
 const post = require('express').Router()
+const postValidator = require('../validators/post/postValidator')
+const upload = require('../middleware/UploadMiddleware')
 
 const {
-    createPostGet
+    createPostGet,
+    createPost,
 } = require('../controllers/postController')
 
-const upload = require('../middleware/UploadMiddleware')
- 
+
 /**
  * All routes for API POST.
  */
-post.get('/createPost',createPostGet)
 
+
+
+post.get('/createPost',createPostGet)
+post.post('/createPost',upload.single('thumbnail'),postValidator,createPost)
  
 module.exports=post
