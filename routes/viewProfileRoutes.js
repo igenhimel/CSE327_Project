08720d882a21bcc1f491d0175a42492805117e66
Alvi Anowar <41396211+alvianowar@users.noreply.github.com>
2021-12-18 20:19:05 +0600

    unnecessary files deleted from branch Alvi

--- routes/viewProfileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req,res) =>
-{
-    res.render('../views/pages/view_and_follow')
-})
-
-module.exports = router;
