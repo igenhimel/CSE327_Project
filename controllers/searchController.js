4015ccb9e69aaf77cff790bcd602aa20215605d9
Alvi Anowar <41396211+alvianowar@users.noreply.github.com>
2021-12-20 17:35:27 +0600

    variable and directory updated

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,12 +2,11 @@ const Post = require('../models/Post')
 
 /**
  * API method for searching post in the database.
- * @param {object} req Request object of the post_search method of controller
- * @param {object} res Response object of the post_search method of controller
+ * @param {object} req Request object of the postSearch method of controller
+ * @param {object} res Response object of the postSearch method of controller
  */
 exports.postSearch = async(req,res) => {
     let searchTerm = req.query.term;
     let post = await Post.find({$text:{ $search: searchTerm, $diacriticSensitive: true}});
     res.render('../views/pages/explore/search', {value: searchTerm, post})
 }
-
