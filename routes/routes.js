ca320c9c7ac46c4010de9bc7ddc81d2e6c036f62
Israkrafi <israkrafi54@gmail.com>
2021-12-22 02:46:08 +0600

    updated by branch israk

--- routes/routes.js
+++ routes/routes.js
@@ -1,33 +1,35 @@
-
 const dashboardRoutes = require('../routes/dashboardRoute')
-
+const profileRoute = require("../routes/CreateProfile")
 const route = [
 
- 
+
     {
-        path:'/dashboard',
-        controller:dashboardRoutes
+        path: '/dashboard',
+        controller: dashboardRoutes
 
     },
     {
-        path:'/',
-        controller:(req,res)=>{
+        path: "/profile",
+        controller: profileRoute
+    },
+    {
+        path: '/',
+        controller: (req, res) => {
 
             res.redirect('/explore')
-           
+
         }
     }
 
 
 ]
 
-module.exports = (app)=>{
-    route.forEach((r)=>{
-       if(r.path=='/'){
-           app.get(r.path,r.controller)
-       }
-       else{
-        app.use(r.path,r.controller)
-       }
+module.exports = (app) => {
+    route.forEach((r) => {
+        if (r.path == '/') {
+            app.get(r.path, r.controller)
+        } else {
+            app.use(r.path, r.controller)
+        }
     })
 }
