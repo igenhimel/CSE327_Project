735f9726696a94506e9a3880a4e97c7605658f02
Israkrafi <israkrafi54@gmail.com>
2021-12-22 02:42:30 +0600

    added by branch israk

---
+++ routes/CreateProfile.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {
+    register
+} = require("../controllers/CreateProfile")
+const CreateProfile = require('./../controllers/CreateProfile')
+router.get('/register', register)
+
+module.exports = router;
