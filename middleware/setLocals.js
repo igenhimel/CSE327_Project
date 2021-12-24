ec08990bae16b3ce3f65358bb5b482c73ec68f80
Habib Al Hasan Himel <djstar.himel@gmail.com>
2021-12-24 21:35:38 +0600

    variable access from view page

--- middleware/setLocals.js
+++ middleware/setLocals.js
@@ -2,33 +2,30 @@ const cheerio = require('cheerio')
 const moment = require('moment')
 
 /**
- * method for global user
- * @returns user is logged in or not
+ * variable are declared globally
+ * @returns post body 
  */
 
 exports.setLocals = () =>{
     return async (req,res,next) =>{
         res.locals.user = req.user
         res.locals.isLoggedIn = req.session.isLoggedIn
-
         res.locals.truncate = html =>{
-            let node = cheerio.load(html)
-            let text = node.text()
+            let node = cheerio.load(html) // cherrio html to text converter
+            let text = node.text() // html to text convert
 
-            text = text.replace(/(\r\n|\n|\r)/gm,'')
+            text = text.replace(/(\r\n|\n|\r)/gm,'') 
 
             if(text.length<=100){
                 return text
-            }
+            } // if text length <=100 then return text
 
             else{
-               return text.substr(0,100) + '...'
+               return text.substr(0,100) + '...' //otherwise text...
             }
         }
-        res.locals.moment = time => moment(time).fromNow()
+        res.locals.moment = time => moment(time).fromNow() //locally declare momentJS variable to access from view
         next()
-
-        
 
     }
 }
