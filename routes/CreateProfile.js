359dd4c29f4795a595de503ab084a72252782dcb
Israkrafi <israkrafi54@gmail.com>
2021-12-24 22:56:42 +0600

    Routes added by israk

--- routes/CreateProfile.js
+++ routes/CreateProfile.js
@@ -3,6 +3,6 @@ const {
     register
 } = require("../controllers/CreateProfile")
 const CreateProfile = require('./../controllers/CreateProfile')
-router.get('/register', register)
+router.get(' /register', register)
 
 module.exports = router;
