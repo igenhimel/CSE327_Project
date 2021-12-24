b99338907fee877fa5d2f755c449bbfe7b53b76a
Israkrafi <israkrafi54@gmail.com>
2021-12-24 22:48:37 +0600

    added by israk

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const setRoutes = require('./routes/routes')
+const setMiddleWare = require('./middleware/middlewares')
 const config = require('config')
 const app = express()
 
+const MONGODB_URI = `mongodb+srv://${config.get('db-admin')}:${config.get('db-password')}@cluster0.13eyw.mongodb.net/CSE327`
 
-const MONGODB_URI=`mongodb+srv://${config.get('db-admin')}:${config.get('db-password')}@cluster0.13eyw.mongodb.net/CSE327`
-
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+/**
+ * Middleware 
+ */
+setMiddleWare(app)
+
+/**
+ * Routes
+ */
+
+setRoutes(app)
 
 
 
 const PORT = process.env.PORT || 3030
 
-mongoose.connect(MONGODB_URI,{useNewUrlParser:true
-})
-.then(()=>{
-        app.listen(PORT,(err)=>{
-        console.log(`Server is Running on port ${PORT}`)
-    })
-})
-.catch((e)=>{
-    console.log(e)
-})
+/**
+ * database connection
+ */
 
-module.exports=app;
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        app.listen(PORT, (err) => {
+            console.log(`Server is Running on port ${PORT}`)
+        })
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+
+module.exports = app;
