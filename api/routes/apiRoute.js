aa1a9c13ae693f603bc72684d3db8fedd6c9ad13
ABmaxplunck <asifulbijoy97@gmail.com>
2021-12-28 20:23:06 +0600

    bookmarks api route updated

--- api/routes/apiRoute.js
+++ api/routes/apiRoute.js
@@ -7,6 +7,14 @@ const {
 
 } = require('../controllers/commentController')
 
+const {
+
+    bookmarksController,
+    bookmarksViewPage
+
+} = require('../controllers/bookmarksController')
+
+const {isAuthenticated} = require('../../middleware/authMiddleware')
 
 /**
  * All comment related api routes
@@ -15,4 +23,11 @@ api.post('/comment/:postId', createCommentPost) // comment route
 
 api.post('/comment/replies/:commentId', repliesPostController) //replies route
 
-module.exports = api //exports api
+/**
+ * all bookmarks related api routes
+ */
+api.get('/bookmarks/:postId',isAuthenticated,bookmarksController)
+api.get('/bookmarks',isAuthenticated,bookmarksViewPage)
+
+module.exports = api
+
